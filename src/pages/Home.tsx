4c7c674bd83f52a1245449a410b2562283679c63
Lucas <lucaslima78@hotmail.com>
2024-11-13 11:15:18 -0300

    up

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,28 @@
+import { Play } from "phosphor-react";
+
 export function Home() {
     return (
-        <h1>Home</h1>
+        <>
+            <form action="">
+                <div>
+                    <label htmlFor="task">vou trabalhar em </label>
+                    <input id="task" />
+                    <label htmlFor="minutesAmount">durante</label>
+                    <input type="number" id="minutesAmount" />
+                    <span>minutos.</span>
+                </div>
+                <>
+                    <span>0</span>
+                    <span>0</span>
+                    <span>:</span>
+                    <span>0</span>
+                    <span>0</span>
+                </>
+                <button type="submit"><Play size={24}/> Começar</button>
+            </form>
+
+
+        </>
 
     );
 }
